47bbe8c105b815b5c4fd20654b9c4b6c28b05c0c
VD-15 <valkyriedev15@gmail.com>
2020-02-03 19:15:02 +0000

    Update mkenv.bat

--- mkenv.bat
+++ mkenv.bat
@@ -5,10 +5,7 @@ git clone https://github.com/glfw/glfw dep/GLFW
 git clone https://github.com/g-truc/glm dep/GLM
 git clone https://github.com/nlohmann/json dep/JSON
 git clone https://github.com/nigels-com/glew dep/GLEW
-
-echo md lib\GLFW\Debug
-echo md lib\GLFW\Release
-echo md inc\GLFW
+git clone https://github.com/nothings/stb dep/stb
 
 echo Building GLFW
 cmake -S dep/GLFW -B dep/GLFW/bin
@@ -16,9 +13,15 @@ cmake --build dep/GLFW/bin --config Debug
 cmake --build dep/GLFW/bin --config Release
 
 echo Copying GLFW
-move /Y dep\GLFW\bin\src\Debug\glfw3.lib lib\GLFW\Debug\glfw3.lib
-move /Y dep\GLFW\bin\src\Release\glfw3.lib lib\GLFW\Release\glfw3.lib
-xcopy dep\GLFW\include\* inc\ /i /e /y
+xcopy dep\GLFW\src\Debug\glfw3.lib lib\GLFW\Debug\glfw3.lib
+xcopy dep\GLFW\bin\src\Release\glfw3.lib lib\GLFW\Release\glfw3.lib
+xcopy dep\GLFW\include\* inc\ /i /y
+
+echo copying GLM
+xcopy dep\GLM\src\* inc\GLM /i /y
+
+echo copying stb
+xcopy dep\stb\stb_image.h inc\stb\stb_image.h
 
 type env\VS_PROJECT > Valkyrie-Engine.vcxproj
 type env\VS_SOLUTION > Valkyrie-Engine.sln

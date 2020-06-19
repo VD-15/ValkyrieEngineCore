8678578d9e93eb84d7ba1edbe97bf3c7c0b5302c
Valkyrie <valkyrie@valkyrie.Home>
2020-06-19 17:04:41 +0100

    Added floating-point typedefs

--- include/ValkyrieEngine/ValkyrieDefs.hpp
+++ include/ValkyrieEngine/ValkyrieDefs.hpp
@@ -4,47 +4,57 @@
 namespace vlk
 {
 	/*!
-	 * Unsigned 8-bit integer
+	 * \brief Unsigned 8-bit integer
 	 */
 	typedef std::uint8_t	UByte;
 
 	/*!
-	 * Signed 8-bit integer
+	 * \brief Signed 8-bit integer
 	 */
 	typedef std::int8_t		Byte;
 
 	/*!
-	 * Unsigned 16-bit integer
+	 * \brief Unsigned 16-bit integer
 	 */
 	typedef std::uint16_t	UShort;
 
 	/*!
-	 * Signed 16-bit integer
+	 * \brief Signed 16-bit integer
 	 */
 	typedef std::int16_t	Short;
 
 	/*!
-	 * Unsigned 32-bit integer
+	 * \brief Unsigned 32-bit integer
 	 */
 	typedef std::uint32_t	UInt;
 
 	/*!
-	 * Signed 32-bit integer
+	 * \brief Signed 32-bit integer
 	 */
 	typedef std::int32_t	Int;
 
 	/*!
-	 * Unsigned 64-bit integer
+	 * \brief Unsigned 64-bit integer
 	 */
 	typedef std::uint64_t	ULong;
 
 	/*!
-	 * Signed 64-bit integer
+	 * \brief Signed 64-bit integer
 	 */
 	typedef std::int64_t	Long;
 
 	/*!
-	 * Platform-dependent size type
+	 * \brief Platform-dependent size type
 	 */
 	typedef std::size_t		Size;
+
+	/*!
+	 * \brief 32-bit floating-point number
+	 */
+	typedef float			Float;
+
+	/*!
+	 * \brief 64-bit floating-point number
+	 */
+	typedef double			Double;
 }

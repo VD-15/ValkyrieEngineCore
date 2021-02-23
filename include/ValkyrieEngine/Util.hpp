374f041ce4b2daf3326bd5f42d3f01be4587b8af
VD-15 <valkyriedev15@gmail.com>
2021-02-23 14:14:56 +0000

    Added Extract parameter template class

--- include/ValkyrieEngine/Util.hpp
+++ include/ValkyrieEngine/Util.hpp
@@ -16,8 +16,27 @@ namespace vlk
 	template <class T, template<class...> class Primary>
 	struct IsSpecialization : std::false_type {};
 
+	/*!
+	 * \copydoc vlk::IsSpecialization
+	 */
 	template <template <class...> class Primary, class... Args>
 	struct IsSpecialization<Primary<Args...>, Primary> : std::true_type {};
+
+	/*!
+	 * \brief Extract parameter template class
+	 *
+	 * Extracts the template parameter of a template type <tt>T</tt>.
+	 * If <tt>T</tt> is not a template, <tt>type<tt> evaluates to <tt>T</tt>.
+	 * If <tt>T</tt> is a template of the form <tt>T<S></tt>, then <tt>type</tt> evaluates to <tt>S</tt>.
+	 */
+	template <typename T>
+	struct ExtractParameter { typedef T type; };
+
+	/*!
+	 * \copydoc vlk::ExtractParameter
+	 */
+	template <template <typename> typename T, typename S>
+	struct ExtractParameter<T<S>> { typedef S type; };
 }
 
 #endif
